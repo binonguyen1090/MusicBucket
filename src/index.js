a77da2a45973fed2f2b9520672130a5640b67015
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-26 22:25:11 -0700

    select query and define sequencer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Webpack is working!");
+function sequencer() {
+    const kick = new Tone.Player('../drums/kick-808.wav').toMaster();
+    const snare = new Tone.Player('../drums/snare-808.wav').toMaster();
+    let i = 0;
+
+    const kickInputs = document.querySelectorAll('.kick');
+    const snareInputs = document.querySelectorAll('.snare');
+}
+sequencer();
