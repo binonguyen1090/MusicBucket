bfbd709b24e5b8e06e13e26200e1d2b5dc71222e
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-29 22:14:25 -0700

    add span running and reset sequence

--- src/sequencer.js
+++ src/sequencer.js
@@ -20,6 +20,9 @@ const play = document.body.querySelector('.play');
 const pause = document.body.querySelector('.pause');
 const rows = document.body.querySelectorAll('.row');
 const refresh = document.body.querySelector('.refresh');
+const reset = document.body.querySelector('.reset');
+const spans = document.body.querySelectorAll('span');
+
 
 
 const notes = ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6'];
@@ -27,8 +30,8 @@ const notes = ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6'];
 let index = 0;
 
 
-Tone.Transport.scheduleRepeat(repeat, "16n")
-
+Tone.Transport.scheduleRepeat(repeat, "8n")
+// Tone.Transport.bpm.value = 20
 play.addEventListener('click', () => {
     Tone.Transport.start()
 })
@@ -42,7 +45,11 @@ refresh.addEventListener('click', e => {
         }
     }
 })
-
+reset.addEventListener('click', e => {
+    index = 0;
+    Tone.Transport.stop();
+    spans.forEach(span => span.classList.remove('highlight'))
+})
 
 function repeat() {
     let step = index % 16;
@@ -51,10 +58,13 @@ function repeat() {
             note = notes[i],
             row = rows[i],
         input = row.querySelector(`label:nth-child(${step + 1})`);
+        span = input.querySelector(`span`);
         checkbox = input.querySelector('input');
         if (checkbox.checked) {
             synth.triggerAttackRelease(note, '8n');  
         }
+        span.classList.toggle('highlight')
+
     }
     index++;
 }
