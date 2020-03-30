fc1bae1d23e7159f6bad2b3272d1362622560fed
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-29 21:13:21 -0700

    style grid

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,3 +1,4 @@
+console.clear()
 document.documentElement.addEventListener('mousedown', () => {
     if (Tone.context.state !== 'running') Tone.context.resume();
 });
@@ -13,27 +14,38 @@ const synths = [
 ]
 
 synths.forEach(synth => synth.toMaster())
+const allInput = document.body.querySelectorAll('input')
 
 const play = document.body.querySelector('.play');
 const pause = document.body.querySelector('.pause');
 const rows = document.body.querySelectorAll('.row');
+const refresh = document.body.querySelector('.refresh');
+
 
 const notes = ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6'];
 
 let index = 0;
 
 
-Tone.Transport.scheduleRepeat(repeat, "8n")
+Tone.Transport.scheduleRepeat(repeat, "16n")
 
-play.addEventListener('click', async () => {
+play.addEventListener('click', () => {
     Tone.Transport.start()
 })
-pause.addEventListener('click', async () => {
+pause.addEventListener('click', () => {
     Tone.Transport.stop()
 })
+refresh.addEventListener('click', e => {
+    for (let i = 0; i < allInput.length; i++) {
+        if (allInput[i].checked === true) {
+            allInput[i].checked = false
+        }
+    }
+})
+
 
 function repeat() {
-    let step = index % 8;
+    let step = index % 16;
     for (let i = 0; i < rows.length; i++) {
         let synth = synths[i],
             note = notes[i],
